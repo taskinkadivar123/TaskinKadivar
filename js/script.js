b0ca2612ef984b817000deb954e6ee2e100e17ac
taskinkadivar123 <taskinkadivar524@gmail.com>
2026-03-27 18:57:29 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const revealItems = document.querySelectorAll(".reveal");
+
+const revealObserver = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("is-visible");
+                revealObserver.unobserve(entry.target);
+            }
+        });
+    },
+    {
+        threshold: 0.18,
+        rootMargin: "0px 0px -40px 0px"
+    }
+);
+
+revealItems.forEach((item) => revealObserver.observe(item));
